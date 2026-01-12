9f98787d6118ea67387ed4857306a0989325d6a1
Saluca <Saluca@users.noreply.github.com>
2026-01-12 22:01:45 +0000

    Delete unused interfaces

--- src/app/types.ts
+++ src/app/types.ts
@@ -6,14 +6,6 @@ export interface CurrentForecast {
   rain: number;
 } // defining object shape TS style, Pascal case
 
-export interface CurrentForecastData {
-  time: string[];
-  temperature_2m: number[];
-  wind_speed_10m: number[];
-  relative_humidity_2m: number[];
-  rain: number[];
-}
-
 export interface HourlyForecast {
   time: string;
   temp: number;
@@ -22,32 +14,10 @@ export interface HourlyForecast {
   rain: number;
 }
 
-export interface HourlyData {
-  time: string[];
-  temperature_2m: number[];
-  wind_speed_10m: number[];
-  relative_humidity_2m: number[];
-  rain: number[];
-}
-
 export interface DailyForecast {
   date: string;
   tempMax: number;
   tempMin: number;
   weathercode: number;
   precipitationProb: number;
-}
-
-export interface DailyData {
-  time: string[];
-  temperature_2m_max: number[];
-  temperature_2m_min: number[];
-  weathercode: number[];
-  precipitation_probability_mean: number[];
-}
-
-export interface ForecastResponse {
-  current_weather: CurrentForecastData;
-  hourly: HourlyData;
-  daily: DailyData;
 }
